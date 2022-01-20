84deb4584c676927f08d58f68a3148cf1db3b9c9
K҉o҉r҉r҉u҉p҉t҉ <28734639+KorruptedKlout@users.noreply.github.com>
2022-01-19 21:50:37 -0800

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     if system == 'nt':
         ctypes.windll.kernel32.SetConsoleTitleW("DBD-Auto-SkillCheck | Made By Rdimo#6969")
     elif system == 'posix':
-        os.system("\033]0;DBD-Auto-SkillCheck | Made By Rdimo#6969\a")
+        os.system("\033]0;DBD-Auto-SkillCheck | Made By ℓσƒι♔נαкє#1442\a")
     sleep(0.5)
     print(f"{Fore.CYAN}Input your key for skill-checking {Fore.RED}")
 
